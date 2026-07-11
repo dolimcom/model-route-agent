3d987f6f7a4e09a6738461516ab2b5c153f9e68b
laiybe <16506608+laiybe@user.noreply.gitee.com>
2026-07-11 14:26:25 +0800

    1day

--- src/main/java/com/modelroute/ModelRouteApplication.java
+++ src/main/java/com/modelroute/ModelRouteApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication
 @EnableConfigurationProperties(ModelRouteProperties.class)
 public class ModelRouteApplication {
-
+/*老吴老吴老吴老吴*/
     public static void main(String[] args) {
         SpringApplication.run(ModelRouteApplication.class, args);
     }
